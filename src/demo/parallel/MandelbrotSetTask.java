068a63cd887e47cf3090d08f2ceb15f1ff84edab
Никита Стук <stuk.nicit@mail.ru>
2025-09-17 10:52:46 +0300

    изменение уравнения

--- src/demo/parallel/MandelbrotSetTask.java
+++ src/demo/parallel/MandelbrotSetTask.java
@@ -275,7 +275,8 @@ class MandelbrotSetTask extends Task<Long> {
         int count = 0;
         Complex c = new Complex(0, 0);
         do {
-            c = c.times(c).plus(comp);
+            Complex cPlusC = comp.plus(comp); // c + c = 2c
+            c = c.times(c).plus(cPlusC); // z = z^2 + 2c
             count++;
         } while (count < CAL_MAX_COUNT && c.lengthSQ() < LENGTH_BOUNDARY);
         return count;
